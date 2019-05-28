002cf49397efd90733e0a288de3868f0c1977872
John B <bohnjailey56@gmail.com>
2019-05-28 11:24:22 -0600

    support for generic types.

--- src/edu/cnm/deepdive/Shuffling.java
+++ src/edu/cnm/deepdive/Shuffling.java
@@ -113,6 +113,21 @@ public class Shuffling {
     }
 
   }
+  public static <T> void shuffle(T[] data) {
+    Random rng = getRandom();
+    shuffle (data, rng);
+
+  }
+
+  public static <T> void shuffle(T[] data, Random rng) {
+    for (int i = data.length - 1; i > 0; i--) {
+      int source = rng.nextInt(i + 1);
+      T temp = data[i];
+      data[i] = data[source];
+      data[source] = temp;
+    }
+
+  }
   protected static Random getRandom(){
     if (defualtRng == null){
       defualtRng = new Random();
